8a6065dfd23f4b6007be8ced249996cd62a4eee4
jags34-ua <123956970+jags34-ua@users.noreply.github.com>
2024-03-20 12:19:03 +0100

    29526264Z: Added program structure

--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -6,7 +6,100 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    class Class2
+    public class ENProduct
     {
+        // Private fields
+        private string code;
+        private string name;
+        private int amount;
+        private float price;
+        private DateTime creationDate;
+
+        // Public properties
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public DateTime CreationDate
+        {
+            get { return creationDate; }
+            set { creationDate = value; }
+        }
+
+        // Default constructor
+        public ENProduct() { }
+
+        // Parameterized constructor
+        public ENProduct(string code, string name, int amount, float price, DateTime creationDate)
+        {
+            this.code = code;
+            this.name = name;
+            this.amount = amount;
+            this.price = price;
+            this.creationDate = creationDate;
+        }
+
+        // Methods
+        public bool Create()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Create(this);
+        }
+
+        public bool Update()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Update(this);
+        }
+
+        public bool Delete()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Delete(this);
+        }
+
+        public bool Read()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.Read(this);
+        }
+
+        public bool ReadFirst()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadFirst(this);
+        }
+
+        public bool ReadNext()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadNext(this);
+        }
+
+        public bool ReadPrev()
+        {
+            CADProduct cadProduct = new CADProduct();
+            return cadProduct.ReadPrev(this);
+        }
     }
 }
