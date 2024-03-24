d936ac54d14756cf538c0fe3904876f28b4953e7
cob19 <cob19@alu.ua.es>
2024-03-24 17:56:47 +0100

    29526536X: added methods and attributes to ENProduct and CADProduct classes,and modified Site1.Master

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    class Class1
+    public class CADProduct
     {
+
+        public string constring
+        {
+            get { return constring; }
+            set { this.constring = value; }
+        }
+
+        public CADProduct()
+        {
+
+        }
+
+        public bool Create(ENProduct en)
+        {
+
+        }
+
+        public bool Update(ENProduct en)
+        {
+
+        }
+
+        public bool Delete(ENProduct en)
+        {
+
+        }
+
+        public bool Read(ENProduct en)
+        {
+
+        }
+
+        public bool ReadFirst(ENProduct en)
+        {
+
+        }
+
+        public bool ReadNext(ENProduct en)
+        {
+
+        }
+
+        public bool ReadPrev(ENProduct en)
+        {
+
+        }
     }
 }
--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -6,7 +6,93 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    class Class2
+    public class ENProduct
     {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Name
+        {
+            get { return _name; }
+            set { this._name = value; }
+        }
+        public string Code
+        {
+            get { return _code; }
+            set { this._code = value; }
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set { this._amount = value; }
+        }
+
+        public float Price
+        {
+            get { return _price; }
+            set { this._price = value; }
+        }
+
+        public int Category
+        {
+            get { return _category; }
+            set { this._category = value; }
+        }
+
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+            set { this._creationDate = value; }
+        }
+
+        public ENProduct()
+        {
+
+        }
+
+        public ENProduct(string code,string name,int amount,float price,DateTime creationDate)
+        {
+
+        }
+
+        public bool Create()
+        {
+
+        }
+
+        public bool Update()
+        {
+
+        }
+
+        public bool Delete()
+        {
+
+        }
+
+        public bool Read()
+        {
+
+        }
+
+        public bool ReadFirst()
+        {
+
+        }
+
+        public bool ReadNext()
+        {
+
+        }
+
+        public bool ReadPrev()
+        {
+
+        }
     }
 }
